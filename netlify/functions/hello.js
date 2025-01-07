31136afbf205f69f6f25741b7e8854bfa343088f
Lee Sang Ho <dmzpl18@gmail.com>
2025-01-07 22:16:13 +0900

    tse

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,7 +1,7 @@
 exports.handler = async () => {
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: "Hello, Netlify Functions!" }),
+      body: JSON.stringify({ message: "Hello, Netlify  111 Functions!" }),
     };
   };
   
